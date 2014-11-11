cb716e44f494ee48b35eb4cbc7e7ff99f19aff6c
Rafael Birkmann <rafkafu@yahoo.de>
2014-11-11 16:56:15 +0100

    subnav

--- source/js/main.js
+++ source/js/main.js
@@ -13,6 +13,16 @@ $(document).ready(function(){
 		};
 	});
 
-	$(".nav-main li:has(ul)").addClass("has-sub-nav").prepend("<div class=\"sub-toggle\"></div>")
+	$(".nav-main li:has(ul)").addClass("has-sub-nav").prepend("<div class=\"sub-toggle\"></div>");
+
+	$(".has-sub-nav a").click(function(){
+		$(this).parent().addClass("active");
+		$(".nav-container").addClass("show-sub");
+	});
+
+	$(".has-sub-nav .back").click(function(){
+		$(".nav-container").removeClass("show-sub");
+		$(".has-sub-nav").removeClass("active");
+	});
 
 });
