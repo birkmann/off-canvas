65eef4e04f9f37f7dedd46b294f46c13c715f63c
Rafael Birkmann <rafkafu@yahoo.de>
2014-11-12 10:54:25 +0100

    close event click main disabled

--- source/js/main.js
+++ source/js/main.js
@@ -1,16 +1,8 @@
 $(document).ready(function(){
-	
+
 	$(".button-nav-toggle").click(function(){
 		$(".main").toggleClass("open");
 		$(".menu").toggleClass("open");
-	});
-
-	$(".main").click(function(){
-		var mainOffset = $(this).css("right").replace('px', '');
-		if (mainOffset > 0) {
-			$(".main").removeClass("open");
-			$(".menu").removeClass("open");
-		};
 	});
 
 	$(".nav-main li:has(ul)").addClass("has-sub-nav").prepend("<div class=\"sub-toggle\"></div>");
@@ -26,3 +18,13 @@ $(document).ready(function(){
 	});
 
 });
+
+/*
+$(document).on('click', ".main.open", function () {
+	var mainOffset = $(this).css("right").replace('px', '');
+	if (mainOffset > 0) {
+		$(".main").removeClass("open");
+		$(".menu").removeClass("open");
+	};
+});
+*/
